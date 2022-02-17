cb24c44389643d96ac72bd941df6946d94dbac59
acalvelli <abbycalvelli@gmail.com>
2022-02-16 20:50:51 -0500

    added server files and react client

---
+++ server/index.js
@@ -0,0 +1,15 @@
+// server/index.js
+
+const express = require("express");
+
+const PORT = 3001;
+
+const app = express();
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
