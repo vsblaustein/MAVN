e9eeb396a1440ccaeca4539fa9b4ae04147ef442
Nathan Yu <nty4@scarletmail.rutgers.edu>
2022-02-22 14:33:29 -0500

    created blank index pages for create, index, join, signup

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ function App() {
   return (
     <Router>
       <ResponsiveAppBar />
+    
     </Router>
   );
 }
---
+++ client/src/pages/signup.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+  
+const Signup = () => {
+  return (
+    <div>
+      <h1>Create a new account</h1>
+    </div>
+  );
+};
+  
+export default Signup;
