35e32894027e39da188787bd948d0155b7808e11
Nathan Yu <nty4@scarletmail.rutgers.edu>
2022-02-22 15:23:02 -0500

    added home page

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,21 @@ import './App.css';
 import ResponsiveAppBar from './components/ResponsiveAppBar';
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route
 } from "react-router-dom";
+
+import Home from './pages';
 
 
 function App() {
   return (
     <Router>
       <ResponsiveAppBar />
-    
+      <Routes>
+        <Route exact path='/' exact element={<Home />} />
+        <Route exact path='/home' exact element={<Home />} />
+      </Routes>
     </Router>
   );
 }
