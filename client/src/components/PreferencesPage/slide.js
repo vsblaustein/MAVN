5761ffd4e63c1951134d759152fe3a700c4c293c
acalvelli <abbycalvelli@gmail.com>
2022-03-25 19:55:01 -0400

    movie tinder on random images

--- client/src/components/PreferencesPage/slide.js
+++ client/src/components/PreferencesPage/slide.js
@@ -5,6 +5,11 @@ import "react-slideshow-image/dist/styles.css";
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import { TextField } from '@mui/material';
+import Axios from 'axios';
+
+// movies title and info loaded from the db
+var movie_posters = [];
+var movie_titles = [];
 
 export default class slides extends React.Component {
     constructor(props) {
@@ -13,67 +18,83 @@ export default class slides extends React.Component {
         this.back = this.back.bind(this);
         this.next = this.next.bind(this);
         this.state = {
-            current: 0,
-            index: 1
+            index: 0,
+            currentImage:movie_posters[0],
+            currentTitle:movie_titles[0],
         };
 
-        console.log("images:" + props.movieImages);
-        // sets the movie slideshow to a current list of movies
-        movie_titles = props.movieTitles;
-        movie_posters = props.movieImages;
+
     }
 
+    // get the movie title and image lists
+    componentDidMount() {
+        // store the list of genres in local storage
+        Axios.get('http://localhost:3001/getMovies', {
+        }).then((response) => {
+            // gives a list of json objects
+            const movies = response.data;
+            const m_title = [];
+            const m_image = [];
+            // for each movie in the JSON 
+            for (const m in movies) {
+                m_title.push(movies[m].title);
+                m_image.push(movies[m].image_path);
+            }
+            console.log("movie titles: " + m_title);
+            console.log("movie image: " + m_image);
+            // set the movie image and title to be used in the slides
+            movie_titles = m_title;
+            movie_posters = m_image;
+        }).catch(err => {
+            console.log(err);
+        });
+
+    };
+
+    // does nothing just goes to next movie
     back() {
-        console.log("do nothing");
         this.next();
     }
 
+    // change the image of the movie tinder
     next() {
-        this.slideRef.current.goNext();
+        // generate random index to make appear
         const num_movies = movie_posters.length;
-        this.setState ({
-            index:(this.state.index + 1) % num_movies
+        const val = Math.floor(Math.random() * num_movies);
+        // sets the current movie title and image to that random index
+        this.setState({
+            index: (this.state.index + val) % num_movies,
+            currentImage:movie_posters[(this.state.index + val) % num_movies],
+            currentTitle:movie_titles[(this.state.index + val) % num_movies],
         });
-        document.getElementById('title').value = movie_titles[this.state.index];
-        console.log('index: ' + this.state.index);
-        console.log(movie_posters[this.state.index]);
+        // console.log('index: ' + this.state.index);
+        // console.log("movie url: " + this.state.currentImage);
+        // console.log("movie title: " + this.state.currentTitle);
     }
 
     render() {
-        const properties = {
-            duration: 5000,
-            autoplay: false,
-            transitionDuration: 500,
-            arrows: false,
-            infinite: true,
-            easing: "ease",
-        };
-
         return (
             <Box className="App">
                 {/* default is first in list */}
-                <br/>
-                <TextField id="title" 
-                inputProps={{ style: {textAlign: 'center', fontSize: 25}, readOnly:true}}
-                InputProps={{disableUnderline:true}}
-                variant='standard' defaultValue={movie_titles[0]} /> <br/> <br/>
+                <br />
+                <TextField id="title"
+                    inputProps={{ style: { textAlign: 'center', fontSize: 25}, readOnly: true }}
+                    InputProps={{ disableUnderline: true }}
+                    sx={{width: '100%'}}
+                    variant='standard' value={this.state.currentTitle}/> <br /> <br />
 
-                <Box className="slide-container"  >
-                    <Slide ref={this.slideRef} {...properties}>
-                        {movie_posters.map((movie, index) => (
-                            <Box key={index} className="each-slide" >
-                                <img className="lazy" src={movie} alt="sample" />
-                            </Box>
-                        ))}
-                    </Slide>
-                </Box> 
+                <Box >
+                    <Box >
+                        <img className="lazy" src={this.state.currentImage} alt="sample" />
+                    </Box>
+                </Box>
 
                 {/* change button watch adds to db, skip does nothing, both advance forward */}
                 <Box className="slide-container buttons">
                     <Button onClick={this.back} sx={{ position: 'absolute', bottom: '5%', left: '13%' }}>
                         Would Skip
                     </Button>
-                    <Button onClick={() => this.next()} sx={{ position: 'absolute', bottom: '5%', right: '10%' }}>
+                    <Button onClick={this.next} sx={{ position: 'absolute', bottom: '5%', right: '10%' }}>
                         Would Watch
                     </Button>
                 </Box>
@@ -82,19 +103,4 @@ export default class slides extends React.Component {
     }
 }
 
-// movies should get loaded from db
-var movie_posters = [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ3XyL74-DQ1iaIm1Gr2D_iAdcmDwPfwvNaA&usqp=CAU',
-    'https://assets.whatsnewonnetflix.com/external_assets/sggkh+%5B%5Blxx*9*8931*07_8_muochl_mvg%5Bwmn%5Bzkr%5Be3%5BC805vQhtDYWV7zJyzMwnXCTFK*B%5BZZZZYueIXLz6VnhF8WggRmyRYgkovJn1up*JjahceXWugYlIo1pRdG4q77A*C0wiRU5CU%5D1FYFEvZaHkac5%5DDkoK6c9NF5R.jpg',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbq0uRkdRYZvm7WKOM5HnwFcTzAD5-vPKw7g&usqp=CAU',
-    'https://i.pinimg.com/originals/d6/cb/6f/d6cb6f2e6c7e4e4f8d0ae094e8032b60.jpg',
-    'https://i.ytimg.com/vi/p0BpMFTYFpU/maxresdefault.jpg'
-];
 
-var movie_titles = [
-    'movie1',
-    'movie2',
-    'movie3',
-    'movie4',
-    'movie5',
-]
