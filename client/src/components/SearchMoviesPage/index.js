ea28ab99af96af2c9edabf311591aab264c6290a
Nathan Yu <nty4@scarletmail.rutgers.edu>
2022-03-25 19:25:56 -0400

    searchquery for title works! need 2 get IDs now

--- client/src/components/SearchMoviesPage/index.js
+++ client/src/components/SearchMoviesPage/index.js
@@ -7,7 +7,7 @@ import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
-import { Link as RouterLink} from 'react-router-dom' ;
+import { Link as RouterLink } from 'react-router-dom';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
@@ -21,24 +21,74 @@ import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import { DatePicker } from '@mui/lab';
 import Axios from 'axios';
 import SearchResultsGrid from '../SearchResultsGrid/SearchResultsGrid';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
+const api_key = "76e275f04f332f92388a49a0a1ad92ee";
 
 const theme = createTheme();
 
 export default function SearchMoviesPage() {
 
-    const [searchResults, setSearchResults] = React.useState("");
+    const [searchResults, setSearchResults] = React.useState(null);
+    const test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
-    const handleSubmit = async(event) => {
+    React.useEffect(() => {console.log("searchResults after change: ", searchResults)}, [searchResults]);
+
+    const fetchMoviesWithTitle = async (url) => {
+        try {
+            const response = await Axios(url);
+            setSearchResults(response.data);
+        } catch(err){
+            console.error(err);
+        }
+    }
+
+    const handleTitleSubmit = async (event) => {
         event.preventDefault();
-        console.log("search button pressed...")
-      };
+
+        
+
+        const data = new FormData(event.currentTarget);
+        const title_search = data.get('title_search');
+        console.log("title button pressed...");
+        console.log("title: ", title_search);
+        /*
+        What needs to be done here?
+        1. get ALL movies that contain title_search as a substring
+        2. for each movie result, grab movie metadata using API QUERY_detail
+        3. populate DB with all of these movies' metadata
+        4. display resulting movies
+        */
+
+
+        const JSON_URL = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=${title_search.replace(' ', '+')}`;
+        console.log(JSON_URL);
+
+        fetchMoviesWithTitle(JSON_URL);
+        
+     
+
+
+
+    };
+
+    const handleActorSubmit = async (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const actor_search = data.get('actor_search'); 
+        console.log("actor button pressed...");
+        console.log("actor: ", actor_search);
+
+
+    };
 
     return (
         <React.Fragment>
             <ResponsiveAppBar />
 
             <ThemeProvider theme={theme}>
-                <Container component="main" maxWidth="xs">
+                <Container component="main" maxWidth="xl">
                     <CssBaseline />
                     <Box
                         sx={{
@@ -54,36 +104,98 @@ export default function SearchMoviesPage() {
                         <Typography component="h1" variant="h5">
                             Movie Search
                         </Typography>
-                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-                            <Grid container spacing={2}>
-                                <Grid item xs={12}>
-                                    <TextField
-                                        required
-                                        fullWidth
-                                        id="user_search"
-                                        label="Title, Actor, Genre"
-                                        name="user_search"
-                                        autoComplete="user_search"
-                                    />
+                        <Grid item xs={3} sm={6}>
+                            <Box component="form" noValidate onSubmit={handleTitleSubmit} sx={{ mt: 1 }}>
+                                <Grid container spacing={2}>
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            required
+                                            fullWidth
+                                            id="title_search"
+                                            label="Title"
+                                            name="title_search"
+                                            autoComplete="title_search"
+                                        />
+                                    </Grid>
                                 </Grid>
-                            </Grid>
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 2, mb: 2 }}
-                            >
-                                Search
-                            </Button>
-
-                        </Box>
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ mt: 2, mb: 2 }}
+                                >
+                                    Search Titles
+                                </Button>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={3} sm={6}>
+                            <Box component="form" noValidate onSubmit={handleActorSubmit} sx={{ mt: 1 }}>
+                                <Grid container spacing={2}>
+                                    <Grid item xs={12}>
+                                        <TextField
+                                            required
+                                            fullWidth
+                                            id="actor_search"
+                                            label="Actor"
+                                            name="actor_search"
+                                            autoComplete="actor_search"
+                                        />
+                                    </Grid>
+                                </Grid>
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ mt: 2, mb: 2 }}
+                                >
+                                    Search Titles for Actor
+                                </Button>
+                            </Box>
+                        </Grid>
                     </Box>
-                </Container>               
+                </Container>
+
+                <Container maxWidth="md">
+                    <ImageList sx={{ width: '100%', height: '100%', padding: 0 }} cols={5} rowHeight={180}>
+                        {itemData.map((item) => (
+                            <ImageListItem key={item.img} sx={{ width: '100%', height: '100%', left: 40, m: '10px' }}>
+                                <img
+                                    src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                                    srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                    alt={item.title}
+                                    loading="lazy"
+                                />
+                            </ImageListItem>
+                        ))}
+                    </ImageList>
+                </Container>
+
+
+
             </ThemeProvider>
-            <SearchResultsGrid
-                movies = {searchResults}
-                setMovies = {setSearchResults}
-             />
         </React.Fragment>
     );
 }
+
+const itemData = [
+    {
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ3XyL74-DQ1iaIm1Gr2D_iAdcmDwPfwvNaA&usqp=CAU',
+        title: 'Genre',
+    },
+    {
+        img: 'https://assets.whatsnewonnetflix.com/external_assets/sggkh+%5B%5Blxx*9*8931*07_8_muochl_mvg%5Bwmn%5Bzkr%5Be3%5BC805vQhtDYWV7zJyzMwnXCTFK*B%5BZZZZYueIXLz6VnhF8WggRmyRYgkovJn1up*JjahceXWugYlIo1pRdG4q77A*C0wiRU5CU%5D1FYFEvZaHkac5%5DDkoK6c9NF5R.jpg',
+        title: 'Length',
+    },
+    {
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbq0uRkdRYZvm7WKOM5HnwFcTzAD5-vPKw7g&usqp=CAU',
+        title: 'Release Year',
+    },
+    {
+        img: 'https://i.pinimg.com/originals/d6/cb/6f/d6cb6f2e6c7e4e4f8d0ae094e8032b60.jpg',
+        title: 'Actors',
+    },
+    {
+        img: 'https://i.ytimg.com/vi/p0BpMFTYFpU/maxresdefault.jpg',
+        title: 'Rating',
+    },
+];
