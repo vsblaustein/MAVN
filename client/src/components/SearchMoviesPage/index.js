75866bbb77b26adf92693b1b8cad3398502f3190
Nathan Yu <nty4@scarletmail.rutgers.edu>
2022-03-28 15:47:26 -0400

    [redacted] titleSearch gets all necessary data now

--- client/src/components/SearchMoviesPage/index.js
+++ client/src/components/SearchMoviesPage/index.js
@@ -30,34 +30,69 @@ const theme = createTheme();
 
 export default function SearchMoviesPage() {
     const [titleSearchResults, setTitleSearchResults] = React.useState(null);
-    var movie_ids = [];
 
-    // this will trigger every time title search results changes- every time search is pressed.
-    //tables being edited: movies, cast_members, movie_genre, actors
-    //movies: title, year, length, image_path, rating, plot
-    //cast_members: title, year, actor, actor_dob
-    //movie_genre: title, year, genre
-    //actors: full_name, first_name, last_name, dob
+    //this object will hold relevant metadata required for DB insertion on title search queries
+    const movie_data = {
+        title: "",
+        year: 0,
+        length: 0,
+        image_path: "https://image.tmdb.org/t/p/w500",
+        rating: 0.0,
+        plot: "",
+        genres: [],
+        actors: [],
+        actor_dobs: []
+    };
+
+    // this will trigger every time title search results changes for re-rendering
     React.useEffect(() => {
     }, [titleSearchResults]);
 
-    const getAllMovieData = async (movie_ids) => {
-        //holds (movie title,year) tuples
-        var result_tuples = [];
-        //for each id, we need to query urls
-        for (var id of movie_ids) {
-            const JSON_URL = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}&language=en-US&append_to_response=credits`;
-            //console.log("URL", JSON_URL);
-            //call axios api to get a json
-            var movie_query_result = await axiosCall(JSON_URL);
-            
+    const getAndSetMovieData = async (id) => {
+        //query movie data along with credits
+        const JSON_URL = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}&language=en-US&append_to_response=credits`;
+        //console.log("URL", JSON_URL);
+
+        //call axios api to get a json
+        var res_json = await axiosCall(JSON_URL);
+        console.log("movie_query_result: ", res_json);
+
+        //set movie_data to whatever we can off of this singular query.
+        const data = Object.create(movie_data);
+        data.title = res_json.title;
+        data.year = res_json.release_date.substring(0,4);
+        data.length = res_json.runtime;
+        data.image_path += res_json.poster_path;
+        data.rating = res_json.vote_average;
+        data.plot = res_json.overview;
+        for (var id of res_json.genres) {
+            data.genres.push(id.name);
         }
+        const movie_cast = res_json.credits.cast;
+        //var actor_ct = 0;
+        for (var actor of movie_cast) {
+            var actor_id = actor.id;
+            data.actors.push(actor);
+            //get actor DOB using actor id
+            const actor_id_json_url = `https://api.themoviedb.org/3/person/${actor_id}?api_key=${api_key}&language=en-US`;
+            var actor_json = await axiosCall(actor_id_json_url);
+            //console.log("actor info: ", actor_json);
+            data.actor_dobs.push(actor_json.birthday);
+            //actor_ct += 1;
+        }
+        console.log("movie_data: ", data);
+        //here's where im supposed to create queries to insert into the respective tables.
+        //query for movies table
+        //query for genres table
+        //query for cast_members table
+        //query for actors table
 
 
-        return result_tuples;
+
+        return [data.title, data.year];
     }
 
-    const getMoviesFromIds = (data) => {
+    const getMovieIDs = (data) => {
         var ids = [];
         for (var movie of data) {
             ids.push(movie.id);
@@ -75,13 +110,13 @@ export default function SearchMoviesPage() {
     }
 
     const handleTitleSubmit = async (event) => {
-        
+
         event.preventDefault();
 
         const data = new FormData(event.currentTarget);
         const title_search = data.get('title_search');
-        console.log("title button pressed...");
-        console.log("title: ", title_search);
+        //console.log("title button pressed...");
+        //console.log("title: ", title_search);
         /*
         What needs to be done here?
         1. get ALL movies that contain title_search as a substring
@@ -92,21 +127,27 @@ export default function SearchMoviesPage() {
 
         //create JSON url for HTTP request
         const JSON_URL = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=${title_search.replace(' ', '+')}`;
-        console.log("JSON URL: ", JSON_URL);
+        //console.log("JSON URL: ", JSON_URL);
         //call API using axios, get results
         var title_api_data = await axiosCall(JSON_URL);
-        console.log("title data: ", title_api_data);
+        //console.log("title data: ", title_api_data);
         //get movie ids
-        var movie_ids = getMoviesFromIds(title_api_data.results);
+        var movie_ids = getMovieIDs(title_api_data.results);
         console.log(movie_ids);
 
         //iterate over movie ids. for each movie id, we need to query and receive:
         //title, year, length, image, rating, plot, genre(s), all actors' names, all actor dobs
-
+        // results will hold the title/year pair of a movie for display purposes
         var results = [];
-        results = await getAllMovieData(movie_ids);
+        for (var id of movie_ids) {
+            var res = await getAndSetMovieData(id);
+            console.log("res: ", res);
+            results.push(res);
+        }
+        console.log("final movie title/year results: ", results);
 
         //after everything, set titleSearchResults to results.
+        setTitleSearchResults(results);
     };
 
     const handleActorSubmit = async (event) => {
